4c71ff073def3b7cc592c9346d12f84b4032ca97
leegue44 <leegy9609@naver.com>
2022-05-31 14:54:38 +0900

    array copy

---
+++ src/arraypratice/ArrayCopy.java
@@ -0,0 +1,16 @@
+package arraypratice;
+
+public class ArrayCopy {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] array1 = {10,20,30,40,50};
+		int[] array2 = {1,2,3,4,5};
+		
+		System.arraycopy(array1, 0, array2, 1, 4);
+					//(복사할배영,첫위치,대상배열,붙여넣을 첫위치,복하샇요소 개수)
+		for(int i:array2)
+			System.out.println(i);
+	}
+
+}
