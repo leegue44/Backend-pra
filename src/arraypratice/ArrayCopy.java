6918502a0be608ff7afa864dee780e758d43c786
leegue44 <leegy9609@naver.com>
2022-05-31 14:55:59 +0900

    수정

--- src/arraypratice/ArrayCopy.java
+++ src/arraypratice/ArrayCopy.java
@@ -8,7 +8,7 @@ public class ArrayCopy {
 		int[] array2 = {1,2,3,4,5};
 		
 		System.arraycopy(array1, 0, array2, 1, 4);
-					//(복사할배영,첫위치,대상배열,붙여넣을 첫위치,복하샇요소 개수)
+					//(복사할배영,첫위치,대상배열,붙여넣을 첫위치,복사할 요소 개수)
 		for(int i:array2)
 			System.out.println(i);
 	}
